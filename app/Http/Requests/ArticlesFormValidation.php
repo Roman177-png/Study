399d0a3815ea42a57d7bfa30225d9999bf2adb7c
roman <Roman177-png>
2019-09-08 23:52:25 +0300

    new

---
+++ app/Http/Requests/ArticlesFormValidation.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ArticlesFormValidation extends FormRequest
+{
+
+    public function authorize()
+    {
+        return true;
+    }
+
+
+    public function rules()
+    {
+        return [
+            'title' => 'required|max:255',
+            'image'  => 'file|mimes:png,jpg,jpeg,gif|max:10240',
+            'description' => 'nullable|max:500'
+
+        ];
+    }
+}
